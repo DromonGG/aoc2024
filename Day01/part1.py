2456358beb28215ddd191be2368ecf6b1ee78493
Dromon <info@dromon.de>
2024-12-02 15:41:20 +0100

    Initial commit

---
+++ Day01/part1.py
@@ -0,0 +1,17 @@
+total = 0
+list_left = []
+list_right = []
+
+with open("Day01/input.txt") as input_file:
+    for line in input_file:
+        numbers = line.strip().split("   ")
+        list_left.append(int(numbers[0]))
+        list_right.append(int(numbers[1]))
+
+list_left.sort()
+list_right.sort()
+
+for left, right in zip(list_left, list_right):
+    total += abs(left - right)
+
+print(total)
---
+++ Day01/part2.py
@@ -0,0 +1,15 @@
+total = 0
+list_left = []
+list_right = []
+
+with open("Day01/input.txt") as input_file:
+    for line in input_file:
+        numbers = line.strip().split("   ")
+        list_left.append(int(numbers[0]))
+        list_right.append(int(numbers[1]))
+
+for number_left in list_left:
+    amount_on_right = list_right.count(number_left)
+    total += number_left * amount_on_right
+
+print(total)
